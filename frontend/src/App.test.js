4377b70e3c7cb1691f60e98bf67c67563c7a085b
Emmanuelaaa <emmanuela_prince@yahoo.com>
2025-08-28 18:14:20 +0100

    Fix frontend test to pass CI

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders frontend API message', () => {
+test('renders App component', () => {
   render(<App />);
-  const messageElement = screen.getByText(/API says:/i);
-  expect(messageElement).toBeInTheDocument();
+  const appElement = screen.getByText(/API says:/i);
+  expect(appElement).toBeInTheDocument();
 });
