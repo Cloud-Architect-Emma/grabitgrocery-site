eb5ecd461bf319015f166bbe97a4398f10368ae8
Emmanuelaaa <emmanuela_prince@yahoo.com>
2025-08-28 13:34:24 +0100

    Fix frontend test to match App.js

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders frontend message', async () => {
+test('renders frontend message', () => {
   render(<App />);
-  const messageElement = await screen.findByText(/hello from frontend/i);
+  const messageElement = screen.getByText(/hello from frontend/i);
   expect(messageElement).toBeInTheDocument();
 });
